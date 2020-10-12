cbd1e1598b0ccb3a9fda76f021a3d67a2ff2445f
Charlotte Downs <charlottedowns@gds10238.lan>
2020-10-12 08:59:01 +0100

    adding js alerts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,30 @@
 /* global $ */
 
+var correct = true,
+    incorrect = false;
+
+function getID(anyID) {
+    return document.getElementById(anyID);
+}
+
+function giveAns(){
+    alert("correct");
+}
+
+/*for (i=0; i<20; i++) {
+    images[i] = i%2===0;
+}*/
+
+window.onload = function(){
+        correct=getID("img1");
+        incorrect=getID("img2");
+        incorrect.onclick = function(){
+            giveAns();
+        }
+        correct.onclick = function() {
+            giveAns();
+        }
+}
 // Warn about using the kit in production
 if (window.console && window.console.info) {
   window.console.info('GOV.UK Prototype Kit - do not use for production')
